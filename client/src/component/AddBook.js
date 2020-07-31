b4676a87790009bbe15338a1c6b938285d8e40ca
ckyyyy <chongkyxxx@gmail.com>
2020-07-31 13:16:17 +0800

    add mutation query and update the state dynamic

--- client/src/component/AddBook.js
+++ client/src/component/AddBook.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getAuthorsQuery } from '../queries/queries';
-
+import { graphql } from 'react-apollo'
+import { flowRight as compose } from 'lodash';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
 class AddBook extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+  }
+
   displayAuthors() {
-    var data = this.props.data;
+    var data = this.props.authors;
     if(data.loading)
     {
       return (<option disabled> Loading... </option>)
@@ -13,36 +22,51 @@ class AddBook extends Component {
     else {
       return (
         data.authors.map(author => {
-          return (<option key={author.id}>{author.name}</option>)
+          return (<option key={author.id} value={author.id}>{author.name}</option>)
         })
       )
     }
+  }
 
+  submitForm(e) {
+    e.preventDefault();
+    this.props.addBook({
+      variables: {
+        name: this.state.name,
+        genre: this.state.genre,
+        authorId: this.state.authorId
+      },
+      refetchQueries: [{ query: getBooksQuery }]
+    })
   }
 
   render() {
     return(
-      <form id="add-book">
-                <div className="field">
-                    <label>Book name:</label>
-                    <input type="text" />
-                </div>
-                <div className="field">
-                    <label>Genre:</label>
-                    <input type="text" />
-                </div>
-                <div className="field">
-                    <label>Author:</label>
-                    <select>
-                        <option>Select author</option>
-                        { this.displayAuthors() }
-                    </select>
-                </div>
-                <button>+</button>
-            </form>
+      <form id="add-book" onSubmit={ this.submitForm.bind(this) }>
+      <h2>Add New Book</h2>
+          <div className="field">
+              <label>Book name:</label>
+              <input type="text" onChange={(e) => this.setState({ name: e.target.value })}/>
+          </div>
+          <div className="field">
+              <label>Genre:</label>
+              <input type="text" onChange={(e) => this.setState({ genre: e.target.value })}/>
+          </div>
+          <div className="field">
+              <label>Author:</label>
+              <select onChange={(e) => this.setState({ authorId: e.target.value })}>
+                  <option>Select author</option>
+                  { this.displayAuthors() }
+              </select>
+          </div>
+          <button>+</button>
+      </form>
     )
   }
 
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "authors" }),
+  graphql(addBookMutation, { name: "addBook" })
+)(AddBook);
